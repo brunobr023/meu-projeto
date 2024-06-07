a311e21bf5ba7881d207e3c692a81f8f03ae9827
Bruno Otavio <100975408+brunobr023@users.noreply.github.com>
2024-06-07 20:00:26 -0300

    Criação de outra pagina

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: 'login.page.html',
+  styleUrls: ['login.page.scss'],
+})
+export class LoginPage {
+  public base!: number;
+  public altura!: number;
+  public resultado!: string;
+  public presentingElement!: any;
+  
+  constructor(private actionSheetCtrl: ActionSheetController) {}
+  
+  public calcularArea() : void{
+    this.resultado = "A área do triangulo é: " + (this.base*this.altura)/ 2 + "cm².";
+  }
+  ngOnInit() {
+    this.presentingElement = document.querySelector('.ion-page');
+  }
+
+  canDismiss = async () => {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Você tem certeza? Isso não guardará as informações.',
+      buttons: [
+        {
+          text: 'Sim',
+          role: 'confirm',
+        },
+        {
+          text: 'Não',
+          role: 'cancel',
+        },
+      ],
+    });
+
+    actionSheet.present();
+
+    const { role } = await actionSheet.onWillDismiss();
+
+    return role === 'confirm';
+  };
+}
