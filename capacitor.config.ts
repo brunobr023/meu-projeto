647098d86813343320687344bf3c35d62a95fca3
brunobr023 <brunootavio326@gmail.com>
2024-06-27 00:46:14 -0300

    pronto

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'meu.projeto',
+  appName: 'meu-projeto',
+  webDir: 'www'
+};
+
+export default config;
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,13 +9,20 @@ import { ActionSheetController } from '@ionic/angular';
 export class HomePage {
   public base!: number;
   public altura!: number;
+  public ladotriangulo1!: number;
+  public ladotriangulo2!: number;
+  public ladotriangulo3!: number;
   public resultado!: string;
   public presentingElement!: any;
+  public modalPerimetroElement!: any;
   
   constructor(private actionSheetCtrl: ActionSheetController) {}
   
   public calcularArea() : void{
     this.resultado = "A área do triangulo é: " + (this.base*this.altura)/ 2 + "cm².";
+  }
+  public calcularPerimetro() : void{
+    this.resultado = "O perimetro do triangulo é: " + (this.ladotriangulo1+this.ladotriangulo2+this.ladotriangulo3) + "cm²";
   }
   ngOnInit() {
     this.presentingElement = document.querySelector('.ion-page');
